83b17270b539498cb74d1a11510af517ce756e60
LUOLIN926 <luolin_0926@icloud.com>
2025-12-02 21:15:13 +0800

    Initial Commit

---
+++ up/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  up
+//
+//  Created by LIN on 2025/12/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ up/upApp.swift
@@ -0,0 +1,17 @@
+//
+//  upApp.swift
+//  up
+//
+//  Created by LIN on 2025/12/2.
+//
+
+import SwiftUI
+
+@main
+struct upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
